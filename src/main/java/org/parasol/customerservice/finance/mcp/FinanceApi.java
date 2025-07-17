6c57365a15b51abaabbf0cbfc93e1c7c692286fe
Bernard Tison <bernard.tison@gmail.com>
2025-07-17 16:53:30 +0200

    Upgrade version of quarkus and quarkus-mcp

--- src/main/java/org/parasol/customerservice/finance/mcp/FinanceApi.java
+++ src/main/java/org/parasol/customerservice/finance/mcp/FinanceApi.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-@ApplicationScoped
 public class FinanceApi {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FinanceApi.class);
